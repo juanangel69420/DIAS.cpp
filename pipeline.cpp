43d120a10ec0881b5c571a429c8470e664aed9ac
Cillian Kelly <cillian.kelly@ucdconnect.ie>
2023-06-21 21:41:33 +0100

    Updated

--- pipeline.cpp
+++ pipeline.cpp
@@ -92,8 +92,6 @@ void create_files(int example_number)
     }
 }
 
-
-
 const int iterations = 3e6;
 const int record = 1000;
 
@@ -111,7 +109,7 @@ int main()
     const double dt = 1e-4;
 
     // Create the files needed for storing arrays
-    const int array_size = 5;
+    const int array_size = 1;
     create_files(array_size);
     std:: fstream file_array[array_size];
 
@@ -137,26 +135,28 @@ int main()
         px1_ds = get_px1_ds(lam1,lam2,Hamiltonian,x1_ds,x2_ds,y1_ds,y2_ds,px2_ds,py1_ds,py2_ds);
 
         // Setup variables for the update function
-        double H_x1_0 = H_x1(lam1,lam2,x1,x2,y1,y2), double H_x1_0_ds = H_x1(lam1,lam2,x1_ds,x2_ds,y1_ds,y2_ds), 
-        double H_x2_0 = H_x2(lam1,lam2,x1,x2,y1,y2), double H_x2_0_ds = H_x2(lam1,lam2,x1_ds,x2_ds,y1_ds,y2_ds), 
-        double H_y1_0 = H_y1(lam1,lam2,x1,x2,y1,y2), double H_y1_0_ds = H_y1(lam1,lam2,x1_ds,x2_ds,y1_ds,y2_ds), 
-        double H_y2_0 = H_y2(lam1,lam2,x1,x2,y1,y2), double H_y2_0_ds = H_y2(lam1,lam2,x1_ds,x2_ds,y1_ds,y2_ds),
+        double H_x1_0 = H_x1(lam1,lam2,x1,x2,y1,y2), H_x1_0_ds = H_x1(lam1,lam2,x1_ds,x2_ds,y1_ds,y2_ds), 
+        H_x2_0 = H_x2(lam1,lam2,x1,x2,y1,y2), H_x2_0_ds = H_x2(lam1,lam2,x1_ds,x2_ds,y1_ds,y2_ds), 
+        H_y1_0 = H_y1(lam1,lam2,x1,x2,y1,y2), H_y1_0_ds = H_y1(lam1,lam2,x1_ds,x2_ds,y1_ds,y2_ds), 
+        H_y2_0 = H_y2(lam1,lam2,x1,x2,y1,y2), H_y2_0_ds = H_y2(lam1,lam2,x1_ds,x2_ds,y1_ds,y2_ds),
 
-        double x1_n = x1 + (px1)*dt - 0.5*(H_x1_0)*pow(dt,2), double x1_n_ds = x1_ds + (px1_ds)*dt - 0.5*(H_x1_0_ds)*pow(dt,2), 
-        double x2_n = x2 + (px2)*dt - 0.5*(H_x2_0)*pow(dt,2), double x2_n_ds = x2_ds + (px2_ds)*dt - 0.5*(H_x2_0_ds)*pow(dt,2), 
-        double y1_n = y1 + (py1)*dt - 0.5*(H_y1_0)*pow(dt,2), double y1_n_ds = y1_ds + (py1_ds)*dt - 0.5*(H_y1_0_ds)*pow(dt,2), 
-        double y2_n = y2 + (py2)*dt - 0.5*(H_y2_0)*pow(dt,2), double y2_n_ds = y2_ds + (py2_ds)*dt - 0.5*(H_y2_0_ds)*pow(dt,2), 
+        x1_n = x1 + (px1)*dt - 0.5*(H_x1_0)*pow(dt,2), x1_n_ds = x1_ds + (px1_ds)*dt - 0.5*(H_x1_0_ds)*pow(dt,2), 
+        x2_n = x2 + (px2)*dt - 0.5*(H_x2_0)*pow(dt,2), x2_n_ds = x2_ds + (px2_ds)*dt - 0.5*(H_x2_0_ds)*pow(dt,2), 
+        y1_n = y1 + (py1)*dt - 0.5*(H_y1_0)*pow(dt,2), y1_n_ds = y1_ds + (py1_ds)*dt - 0.5*(H_y1_0_ds)*pow(dt,2), 
+        y2_n = y2 + (py2)*dt - 0.5*(H_y2_0)*pow(dt,2), y2_n_ds = y2_ds + (py2_ds)*dt - 0.5*(H_y2_0_ds)*pow(dt,2), 
 
-        double H_x1_n = H_x1(lam1,lam2,x1_n,x2_n,y1_n,y2_n), double H_x1_n_ds = H_x1(lam1,lam2,x1_n_ds,x2_n_ds,y1_n_ds,y2_n_ds),
-        double H_x2_n = H_x2(lam1,lam2,x1_n,x2_n,y1_n,y2_n), double H_x2_n_ds = H_x2(lam1,lam2,x1_n_ds,x2_n_ds,y1_n_ds,y2_n_ds),
-        double H_y1_n = H_y1(lam1,lam2,x1_n,x2_n,y1_n,y2_n), double H_y1_n_ds = H_y1(lam1,lam2,x1_n_ds,x2_n_ds,y1_n_ds,y2_n_ds),
-        double H_y2_n = H_y2(lam1,lam2,x1_n,x2_n,y1_n,y2_n), double H_y2_n_ds = H_y2(lam1,lam2,x1_n_ds,x2_n_ds,y1_n_ds,y2_n_ds);
+        H_x1_n = H_x1(lam1,lam2,x1_n,x2_n,y1_n,y2_n), H_x1_n_ds = H_x1(lam1,lam2,x1_n_ds,x2_n_ds,y1_n_ds,y2_n_ds),
+        H_x2_n = H_x2(lam1,lam2,x1_n,x2_n,y1_n,y2_n), H_x2_n_ds = H_x2(lam1,lam2,x1_n_ds,x2_n_ds,y1_n_ds,y2_n_ds),
+        H_y1_n = H_y1(lam1,lam2,x1_n,x2_n,y1_n,y2_n), H_y1_n_ds = H_y1(lam1,lam2,x1_n_ds,x2_n_ds,y1_n_ds,y2_n_ds),
+        H_y2_n = H_y2(lam1,lam2,x1_n,x2_n,y1_n,y2_n), H_y2_n_ds = H_y2(lam1,lam2,x1_n_ds,x2_n_ds,y1_n_ds,y2_n_ds);
 
+        //Initialize first elements of solution arrays (Necessary to make update rule more efficient)
         x1_sol[0] = x1, x2_sol[0] = x2, y1_sol[0] = y1, y2_sol[0] = y2,
         px1_sol[0] = px1, px2_sol[0] = px2, py1_sol[0] = py1, py2_sol[0] = py2;
 
         x1_sol_ds[0] = x1_ds, x2_sol_ds[0] = x2_ds, y1_sol_ds[0] = y1_ds, y2_sol_ds[0] = y2_ds,
         px1_sol_ds[0] = px1_ds, px2_sol_ds[0] = px2_ds, py1_sol_ds[0] = py1_ds, py2_sol_ds[0] = py2_ds;
+
 
         for (int i = 0; i < iterations; i++)
         {
@@ -171,15 +171,52 @@ int main()
                 px1_sol_ds[i / record] = px1_ds, px2_sol_ds[i / record] = px2_ds, py1_sol_ds[i / record] = py1_ds, py2_sol_ds[i / record] = py2_ds;
             }
         }
-    }
 
-    for (int i=0; i<array_size/*In future you may need to divide array size by 2 since files only indexed up to 20*/; i++)
-    {
-        std:: string path = "C:/Users/cilli/VS programmes/Run_files1.0/run_" + std::to_string(i+1) + ".txt";
-        file_array[i].open(path, std::ios::out);
+        // Create paths and output files
+        std:: string path_out = "C:/Users/cilli/DIAS.cpp/Run_files1.0/run_" + std::to_string(i+1) + ".txt";
+        std:: string path_out_ds = "C:/Users/cilli/DIAS.cpp/Run_files1.0/run_ds" + std::to_string(i+1) + ".txt";
+        std:: fstream file_out(path_out,std::ios::out);
+        std:: fstream file_out_ds(path_out_ds,std::ios::out);
+
         for (int i=0; i < iterations / record; i++)
         {
-            file_array[i] << x1_sol[i] << ",";
+            file_out << x1_sol[i] << ",";
+            file_out_ds << x1_sol_ds[i] << ",";
+        }
+        for (int i=0; i < iterations / record; i++)
+        {
+            file_out << x2_sol[i] << ",";
+            file_out_ds << x2_sol_ds[i] << ",";
+        }
+        for (int i=0; i < iterations / record; i++)
+        {
+            file_out << y1_sol[i] << ",";
+            file_out_ds << y1_sol_ds[i] << ",";
+        }
+        for (int i=0; i < iterations / record; i++)
+        {
+            file_out << y2_sol[i] << ",";
+            file_out_ds << y2_sol_ds[i] << ",";
+        }
+        for (int i=0; i < iterations / record; i++)
+        {
+            file_out << px1_sol[i] << ",";
+            file_out_ds << px1_sol_ds[i] << ",";
+        }
+        for (int i=0; i < iterations / record; i++)
+        {
+            file_out << px2_sol[i] << ",";
+            file_out_ds << px2_sol_ds[i] << ",";
+        }
+        for (int i=0; i < iterations / record; i++)
+        {
+            file_out << py1_sol[i] << ",";
+            file_out_ds << py1_sol_ds[i] << ",";
+        }
+        for (int i=0; i < iterations / record; i++)
+        {
+            file_out << py2_sol[i] << ",";
+            file_out_ds << py2_sol_ds[i] << ",";
         }
     }
 }
