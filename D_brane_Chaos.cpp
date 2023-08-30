b8dfe452fc73f7c782a79f6d2bf9dccc0b119ff5
Cillian Kelly <cillian.kelly@ucdconnect.ie>
2023-08-30 22:02:09 +0100

    asdf

--- D_brane_Chaos.cpp
+++ D_brane_Chaos.cpp
@@ -22,15 +22,41 @@ of the complex valued degrees of freedom.
 N.B. The X matrices should be hermitian but they need not be traceless in this case.  
 */
 
-// Scalar Z's
-double P12, P13, P21, P23, P31, P32;
-double Q12, Q13, Q21, Q23, Q31, Q32;
+std:: mt19937 rng(std::time(nullptr));
+std:: normal_distribution<long double> gauss_dist(0, 1);
+
+// Initialise everything as a random variable for now until swapno gets back and says what things should be
+// Scalar Z's 
+double P12 = gauss_dist(rng), P13 = gauss_dist(rng), P21 = gauss_dist(rng), P23 = gauss_dist(rng), P31 = gauss_dist(rng), P32 = gauss_dist(rng);
+double Q12 = gauss_dist(rng), Q13 = gauss_dist(rng), Q21 = gauss_dist(rng), Q23 = gauss_dist(rng), Q31 = gauss_dist(rng), Q32 = gauss_dist(rng);
 complex Z12 = complex(P12,Q12);
 complex Z13 = complex(P13,Q13);
 complex Z21 = complex(P21,Q21);
 complex Z23 = complex(P23,Q23); 
 complex Z31 = complex(P31,Q31); 
 complex Z32 = complex(P32,Q32); 
+
+// Vector Z's
+Eigen::Matrix<double, N, 1> P41 = Eigen::Matrix<double, N, 1>::Random();
+Eigen::Matrix<double, N, 1> P42 = Eigen::Matrix<double, N, 1>::Random();
+Eigen::Matrix<double, N, 1> P43 = Eigen::Matrix<double, N, 1>::Random();
+Eigen::Matrix<double, N, 1> Q41 = Eigen::Matrix<double, N, 1>::Random();
+Eigen::Matrix<double, N, 1> Q42 = Eigen::Matrix<double, N, 1>::Random(); 
+Eigen::Matrix<double, N, 1> Q43 = Eigen::Matrix<double, N, 1>::Random();
+
+Eigen::Matrix<double, 1, N> P14 = Eigen::Matrix<double, N, 1>::Random();
+Eigen::Matrix<double, N, 1> P24 = Eigen::Matrix<double, N, 1>::Random(); 
+Eigen::Matrix<double, N, 1> P34 = Eigen::Matrix<double, N, 1>::Random();
+Eigen::Matrix<double, N, 1> Q14 = Eigen::Matrix<double, N, 1>::Random(); 
+Eigen::Matrix<double, N, 1> Q24 = Eigen::Matrix<double, N, 1>::Random();
+Eigen::Matrix<double, N, 1> Q34 = Eigen::Matrix<double, N, 1>::Random();
+
+Eigen::Matrix<std::complex<double>, N, 1> Z41 = P41 + complex(0,1)*Q41;
+Eigen::Matrix<std::complex<double>, N, 1> Z42 = P42 + complex(0,1)*Q42;
+Eigen::Matrix<std::complex<double>, N, 1> Z43 = P43 + complex(0,1)*Q43;
+Eigen::Matrix<std::complex<double>, 1, N> Z14 = P14 + complex(0,1)*Q14;
+Eigen::Matrix<std::complex<double>, 1, N> Z24 = P24 + complex(0,1)*Q24;
+Eigen::Matrix<std::complex<double>, 1, N> Z34 = P34 + complex(0,1)*Q34;
 
 // Scalar X's
 complex X11, X12, X13, X14, X15, X16, X17, X18, X19;
@@ -40,13 +66,7 @@ complex X31, X32, X33, X34, X35, X36, X37, X38, X39;
 // Matrix X's
 matrix X41, X42, X43, X44, X45, X46, X47, X48, X49;
 
-// Vector Z's
-Eigen::Matrix<std::complex<double>, N, 1> Z41, Z42, Z43;
-Eigen::Matrix<std::complex<double>, 1, N> Z14, Z24, Z34;
-
 int main()
 {
-    P12 = 2;
-    std:: cout << Z12;
     return 0;
 }
