a7eab64ccb8014f572fefd222ad18cca6b9e1c4a
juanangel69420 <90901287+juanangel69420@users.noreply.github.com>
2024-01-06 20:09:28 +0000

    V_D workingish

--- V_D.cpp
+++ V_D.cpp
@@ -34,6 +34,41 @@ double modsqr(complex a)
     return pow(abs(a),2);
 }
 
+// Kinetic energy with the Z coordinates only 
+
+double K(
+    complex U12, complex U13, complex U21, complex U23, complex U31, complex U32, row U14, row U24, row U34, col U41, col U42, col U43)
+{
+    double scalar_sum = modsqr(U12) + modsqr(U13) + modsqr(U21) + modsqr(U23) + modsqr(U31) + modsqr(U32);
+    double row_sum = (U14.adjoint()*U14 + U24.adjoint()*U24 + U34.adjoint()*U34).trace().real();
+    double col_sum = (U41.adjoint()*U41 + U42.adjoint()*U42 + U43.adjoint()*U43).trace().real();
+    return 0.5 * (scalar_sum + row_sum + col_sum);
+}
+
+double V_D(
+    complex Z12, complex Z13, complex Z21, complex Z23, complex Z31, complex Z32, row Z14, row Z24, row Z34, col Z41, col Z42, col Z43,
+    double c1, double c2, double c3, double c4)
+{
+    // For k = 1
+    double sum1 = pow(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) - c1/g*g, 2);
+    
+    // For k = 2
+    double sum2 = pow(modsqr(Z21) + modsqr(Z23) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/g*g, 2);
+    
+    // For k = 3
+    double sum3 = pow(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/g*g, 2);
+
+    // For k = 4
+    double sum4 = ((
+        Z41*Z41.adjoint() + Z42*Z42.adjoint() + Z43*Z43.adjoint() - Z14.adjoint()*Z14 - Z24.adjoint()*Z24 - Z34.adjoint()*Z34 - 
+        (c4/g*g) * matrix::Identity()
+        )*(
+        Z41*Z41.adjoint() + Z42*Z42.adjoint() + Z43*Z43.adjoint() - Z14.adjoint()*Z14 - Z24.adjoint()*Z24 - Z34.adjoint()*Z34 - 
+        (c4/g*g) * matrix::Identity()
+        )).trace().real();    
+        
+    return 0.5 * (sum1 + sum2 + sum3 + sum4);
+}
 std:: mt19937 rng(std::time(nullptr));
 std:: normal_distribution<long double> gauss_dist(0, 1);
 
@@ -49,8 +84,8 @@ col Z41 = col::Random();
 col Z42 = col::Random();
 col Z43 = col::Random();
 row Z14 = row::Random();
-row Z24 = row::Random(); 
-row Z34 = row::Random();
+row Z24 = /*row::Zero();*/row::Random(); 
+row Z34 = row::Zero();//row::Random();
 
 // initialise the Z_n'
 
@@ -84,15 +119,15 @@ row U24 = row::Zero();
 row U34 = row::Zero();
 
 // Initialise c's as complex but they may be real ask Swapno if things don't work
-complex c1 = complex(gauss_dist(rng),gauss_dist(rng));
-complex c2 = complex(gauss_dist(rng),gauss_dist(rng));
-complex c3 = complex(gauss_dist(rng),gauss_dist(rng));
-complex c4 = complex(gauss_dist(rng),gauss_dist(rng));
+double c1 = 2; //gauss_dist(rng);
+double c2 = 3; //gauss_dist(rng);
+double c3 = 0; //gauss_dist(rng);
+double c4 = 0; //gauss_dist(rng);
 
 // Define the Energy 
-double H()
+double H(double K, double V_D)
 {
-    double E;
+    double E = K + V_D;
     return E;
 }
 
@@ -102,102 +137,102 @@ complex FZ12(
     complex Z12, complex Z13, row Z14, complex Z21, complex Z31, col Z41,
     complex Z23, row Z24, complex Z32, col Z42)
 {
-    return Z12*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) - c1/(g*g))
-    - Z12*(modsqr(Z21) + modsqr(Z23) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g));
+    return complex(2,0) * (Z12*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) - c1/(g*g))
+    - Z12*(modsqr(Z21) + modsqr(Z23) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g)));
 }
 
 complex FZ21(
     complex Z12, complex Z13, row Z14, complex Z21, complex Z31, col Z41,
     complex Z23, row Z24, complex Z32, col Z42)
 {
-    return - Z21*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) - c1/(g*g))
-    + Z21*(modsqr(Z21) + modsqr(Z23) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g));
+    return complex(2,0) * (- Z21*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) - c1/(g*g))
+    + Z21*(modsqr(Z21) + modsqr(Z23) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g)));
 }
 
 complex FZ13(
     complex Z12, complex Z13, row Z14, complex Z21, complex Z31, col Z41,
     complex Z23, row Z34, complex Z32, col Z43)
 {
-    return Z13*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) - c1/(g*g))
-    - Z13*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g)); 
+    return complex(2,0) * (Z13*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) - c1/(g*g))
+    - Z13*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g))); 
 }
 
 complex FZ31(
     complex Z12, complex Z13, row Z14, complex Z21, complex Z31, col Z41,
     complex Z23, row Z34, complex Z32, col Z43)
 {
-    return - Z31*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) - c1/(g*g))
-    + Z31*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g));
+    return complex(2,0) * (- Z31*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) - c1/(g*g))
+    + Z31*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g)));
 }
 
 complex FZ23(
     complex Z23, complex Z21, row Z24, complex Z12, complex Z32, col Z42,
     complex Z31, row Z34, complex Z13, col Z43)
 {
-    return Z23*(modsqr(Z23) + modsqr(Z21) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g))
-    - Z23*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g));
+    return complex(2,0) * (Z23*(modsqr(Z23) + modsqr(Z21) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g))
+    - Z23*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g)));
 }
 
 complex FZ32(
     complex Z23, complex Z21, row Z24, complex Z12, complex Z32, col Z42,
     complex Z31, row Z34, complex Z13, col Z43)
 {
-    return - Z32*(modsqr(Z23) + modsqr(Z21) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g))
-    + Z32*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g));
+    return complex(2,0) * (- Z32*(modsqr(Z23) + modsqr(Z21) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g))
+    + Z32*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g)));
 }
 
 row FZ14(
     row Z14, complex Z12, complex Z13, complex Z21, complex Z31, col Z41, 
     col Z42, col Z43, row Z24, row Z34)
 {
-    return Z14*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) -c1/(g*g))
+    return complex(2,0) * (Z14*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) -c1/(g*g))
     - Z14*(Z41*Z41.adjoint() + Z42*Z42.adjoint() + Z43*Z43.adjoint() - Z14.adjoint()*Z14 - Z24.adjoint()*Z24 - Z34.adjoint()*Z34
-    - c4/(g*g) * matrix::Identity()); 
+    - c4/(g*g) * matrix::Identity())); 
 }
 
 col FZ41(
     row Z14, complex Z12, complex Z13, complex Z21, complex Z31, col Z41, 
     col Z42, col Z43, row Z24, row Z34)
 {
-    return - Z41*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) -c1/(g*g))
+    return complex(2,0) * (- Z41*(modsqr(Z12) + modsqr(Z13) + rowabsqr(Z14) - modsqr(Z21) - modsqr(Z31) - colabsqr(Z41) -c1/(g*g))
     + (Z41*Z41.adjoint() + Z42*Z42.adjoint() + Z43*Z43.adjoint() - Z14.adjoint()*Z14 - Z24.adjoint()*Z24 - Z34.adjoint()*Z34
-    - c4/(g*g) * matrix::Identity())*Z41;
+    - c4/(g*g) * matrix::Identity())*Z41);
 }
 
 row FZ24(
     row Z24, complex Z21, complex Z23, complex Z12, complex Z32, col Z41,
     col Z42, col Z43, row Z14, row Z34)
 {
-    return Z24*(modsqr(Z21) + modsqr(Z23) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g))
+    return complex(2,0) * (Z24*(modsqr(Z21) + modsqr(Z23) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g))
     - Z24*(Z41*Z41.adjoint() + Z42*Z42.adjoint() + Z43*Z43.adjoint() - Z14.adjoint()*Z14 - Z24.adjoint()*Z24 - Z34.adjoint()*Z34
-    - c4/(g*g) * matrix::Identity());
+    - c4/(g*g) * matrix::Identity()));
 }
 
 col FZ42(
     row Z24, complex Z21, complex Z23, complex Z12, complex Z32, col Z41,
     col Z42, col Z43, row Z14, row Z34)
 {
-    return - Z42*(modsqr(Z21) + modsqr(Z23) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g))
+    return complex(2,0) * (- Z42*(modsqr(Z21) + modsqr(Z23) + rowabsqr(Z24) - modsqr(Z12) - modsqr(Z32) - colabsqr(Z42) - c2/(g*g))
     + (Z41*Z41.adjoint() + Z42*Z42.adjoint() + Z43*Z43.adjoint() - Z14.adjoint()*Z14 - Z24.adjoint()*Z24 - Z34.adjoint()*Z34
-    - c4/(g*g) * matrix::Identity())*Z42;
+    - c4/(g*g) * matrix::Identity())*Z42);
 }
 
 row FZ34(
     row Z34, complex Z31, complex Z32, complex Z13, complex Z23, col Z41,
     col Z42, col Z43, row Z14, row Z24)
 {
-    return Z34*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g))
+    return complex(2,0) * (Z34*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g))
     - Z34*(Z41*Z41.adjoint() + Z42*Z42.adjoint() + Z43*Z43.adjoint() - Z14.adjoint()*Z14 - Z24.adjoint()*Z24 - Z34.adjoint()*Z34
-    - c4/(g*g) * matrix::Identity());
+    - c4/(g*g) * matrix::Identity()));
 }
 
 col FZ43(
     row Z34, complex Z31, complex Z32, complex Z13, complex Z23, col Z41,
     col Z42, col Z43, row Z14, row Z24)
 {
-    return - Z43*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g))
+    return complex(2,0) * (- Z43*(modsqr(Z31) + modsqr(Z32) + rowabsqr(Z34) - modsqr(Z13) - modsqr(Z23) - colabsqr(Z43) - c3/(g*g))
     + (Z41*Z41.adjoint() + Z42*Z42.adjoint() + Z43*Z43.adjoint() - Z14.adjoint()*Z14 - Z24.adjoint()*Z24 - Z34.adjoint()*Z34
-    - c4/(g*g) * matrix::Identity())*Z43;
+    - c4/(g*g) * matrix::Identity())*Z43);
 }
 
 // Define the Update function... Huge scope for error here make sure that all of the paramaters of the force functions are in the 
@@ -216,7 +251,7 @@ void update(
     *Z21_n = *Z21 + *U21*dt - 0.5*FZ21(*Z12, *Z13, *Z14, *Z21, *Z31, *Z41, *Z23, *Z24, *Z32, *Z42)*pow(dt,2);
     *Z23_n = *Z23 + *U23*dt - 0.5*FZ23(*Z23, *Z21, *Z24, *Z12, *Z32, *Z42, *Z31, *Z24, *Z13, *Z43)*pow(dt,2);
     *Z31_n = *Z31 + *U31*dt - 0.5*FZ31(*Z12, *Z13, *Z14, *Z21, *Z31, *Z41, *Z23, *Z34, *Z32, *Z43)*pow(dt,2);
-    *Z32_n = *Z32 + *U32*dt - 0.5*FZ32(*Z23, *Z21, *Z24, *Z12, *Z32, *Z42, *Z31, *Z34, *Z31, *Z43)*pow(dt,2);
+    *Z32_n = *Z32 + *U32*dt - 0.5*FZ32(*Z23, *Z21, *Z24, *Z12, *Z32, *Z42, *Z31, *Z34, *Z13, *Z43)*pow(dt,2);
     *Z14_n = *Z14 + *U14*dt - 0.5*FZ14(*Z14, *Z12, *Z13, *Z21, *Z31, *Z41, *Z42, *Z43, *Z24, *Z34)*pow(dt,2);
     *Z24_n = *Z24 + *U24*dt - 0.5*FZ24(*Z24, *Z21, *Z23, *Z12, *Z32, *Z41, *Z42, *Z43, *Z14, *Z34)*pow(dt,2);
     *Z34_n = *Z34 + *U34*dt - 0.5*FZ34(*Z34, *Z31, *Z32, *Z13, *Z23, *Z41, *Z42, *Z43, *Z14, *Z24)*pow(dt,2);
@@ -227,9 +262,9 @@ void update(
     *U12 = *U12 - 0.5*(FZ12(*Z12, *Z13, *Z14, *Z21, *Z31, *Z41, *Z23, *Z24, *Z32, *Z42) + FZ12(*Z12_n, *Z13_n, *Z14_n, *Z21_n, *Z31_n, *Z41_n, *Z23_n, *Z24_n, *Z32_n, *Z42_n))*dt;
     *U13 = *U13 - 0.5*(FZ13(*Z12, *Z13, *Z14, *Z21, *Z31, *Z41, *Z23, *Z34, *Z32, *Z43) + FZ13(*Z12_n, *Z13_n, *Z14_n, *Z21_n, *Z31_n, *Z41_n, *Z23_n, *Z34_n, *Z32_n, *Z43_n))*dt;
     *U21 = *U21 - 0.5*(FZ21(*Z12, *Z13, *Z14, *Z21, *Z31, *Z41, *Z23, *Z24, *Z32, *Z42) + FZ21(*Z12_n, *Z13_n, *Z14_n, *Z21_n, *Z31_n, *Z41_n, *Z23_n, *Z24_n, *Z32_n, *Z42_n))*dt;
-    *U23 = *U23 - 0.5*(FZ23(*Z23, *Z21, *Z24, *Z12, *Z32, *Z42, *Z31, *Z24, *Z13, *Z43) + FZ23(*Z23_n, *Z21_n, *Z24_n, *Z12_n, *Z32_n, *Z42_n, *Z31_n, *Z24_n, *Z13_n, *Z43_n))*dt;
-    *U31 = *U31 - 0.5*(FZ31(*Z12, *Z13, *Z14, *Z21, *Z31, *Z41, *Z23, *Z34, *Z32, *Z43) + FZ31(*Z23_n, *Z21_n, *Z24_n, *Z12_n, *Z32_n, *Z42_n, *Z31_n, *Z24_n, *Z13_n, *Z43_n))*dt;
-    *U32 = *U32 - 0.5*(FZ32(*Z23, *Z21, *Z24, *Z12, *Z32, *Z42, *Z31, *Z34, *Z31, *Z43) + FZ32(*Z23_n, *Z21_n, *Z24_n, *Z12_n, *Z32_n, *Z42_n, *Z31_n, *Z34_n, *Z31_n, *Z43_n))*dt;
+    *U23 = *U23 - 0.5*(FZ23(*Z23, *Z21, *Z24, *Z12, *Z32, *Z42, *Z31, *Z24, *Z13, *Z43) + FZ23(*Z23_n, *Z21_n, *Z24_n, *Z12_n, *Z32_n, *Z42_n, *Z31_n, *Z24_n, *Z13_n, *Z43_n))*dt; 
+    *U31 = *U31 - 0.5*(FZ31(*Z12, *Z13, *Z14, *Z21, *Z31, *Z41, *Z23, *Z34, *Z32, *Z43) + FZ31(*Z12_n, *Z13_n, *Z14_n, *Z21_n, *Z31_n, *Z41_n, *Z23_n, *Z34_n, *Z32_n, *Z43_n))*dt;
+    *U32 = *U32 - 0.5*(FZ32(*Z23, *Z21, *Z24, *Z12, *Z32, *Z42, *Z31, *Z34, *Z13, *Z43) + FZ32(*Z23_n, *Z21_n, *Z24_n, *Z12_n, *Z32_n, *Z42_n, *Z31_n, *Z34_n, *Z13_n, *Z43_n))*dt;
     *U14 = *U14 - 0.5*(FZ14(*Z14, *Z12, *Z13, *Z21, *Z31, *Z41, *Z42, *Z43, *Z24, *Z34) + FZ14(*Z14_n, *Z12_n, *Z13_n, *Z21_n, *Z31_n, *Z41_n, *Z42_n, *Z43_n, *Z24_n, *Z34_n))*dt;
     *U24 = *U24 - 0.5*(FZ24(*Z24, *Z21, *Z23, *Z12, *Z32, *Z41, *Z42, *Z43, *Z14, *Z34) + FZ24(*Z24_n, *Z21_n, *Z23_n, *Z12_n, *Z32_n, *Z41_n, *Z42_n, *Z43_n, *Z14_n, *Z34_n))*dt;
     *U34 = *U34 - 0.5*(FZ34(*Z34, *Z31, *Z32, *Z13, *Z23, *Z41, *Z42, *Z43, *Z14, *Z24) + FZ34(*Z34_n, *Z31_n, *Z32_n, *Z13_n, *Z23_n, *Z41_n, *Z42_n, *Z43_n, *Z14_n, *Z24_n))*dt;
@@ -266,9 +301,7 @@ col Z43_sol[iterations/1000];
 
 int main()
 {
-    int p = 1;
-    float q = 2;
-    std::cout << (p/q) << std:: endl;
+
     for (int i = 0; i < iterations; i++)
     {
         if (i % 1000 == 0)
@@ -286,14 +319,15 @@ int main()
             Z42_sol[i/1000] = Z42;
             Z43_sol[i/1000] = Z43;
 
-            std::cout << i/iterations << "%" << std::endl;
+            std::cout << K(U12,U13,U21,U23,U31,U32,U14,U24,U34,U41,U42,U43)+V_D(Z12,Z13,Z21,Z23,Z31,Z32,Z14,Z24,Z34,Z41,Z42,Z43,c1,c2,c3,c4) << std::endl;
+            std::cout << "Z12 = " << Z12 << std::endl << "Z13 = " << Z13 << std::endl << "Z21 = "<< Z21 << std::endl;
         }
-        
+
         update(
             dt, &Z12, &Z12_n, &Z21, &Z21_n, &Z13, &Z13_n, &Z31, &Z31_n, &Z23, &Z23_n, 
             &Z32, &Z32_n, &Z41, &Z41_n, &Z42, &Z42_n, &Z43, &Z43_n, &Z14, &Z14_n, 
             &Z24, &Z24_n, &Z34, &Z34_n, &U12, &U13, &U21, &U23, &U31, &U32, &U14, &U24, &U34, &U41, &U42, &U43);
-
+    
     }
     return 0;
 }
--- testing.cpp
+++ testing.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+#include "eigen/Eigen/Dense"
 
 int main()
 {
-    int a = 1;
-    std:: cout << a;
+    std::complex<double> a = std::complex<double> (2,3);
+    std::cout << a;
     return 0;
 }
